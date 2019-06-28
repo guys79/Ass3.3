f83d2cb012d5b9dcc55c90cd91983183c72b7cc2
guys79 <38560960+guys79@users.noreply.github.com>
2019-06-28 21:36:42 +0300

    Add files via upload
    added dynamic addition of checked button

--- javascript/validate_sign_up.js
+++ javascript/validate_sign_up.js
@@ -9,24 +9,26 @@ $(document).ready(function () {
     jQuery.validator.addMethod("letters", function(value, element) {
         return this.optional(element) || /^[a-zA-Z]+$/.test(value);
     }, "Please enter only letters");
-    jQuery.validator.addMethod("unique", function(value, element) {
-        for(var i=0;i<usersArr.length;i++)
-        {
-            if(usersArr[i].username === value)
-                return false;
-        }
-        return true;
-    }, "This username is already being used");
+    jQuery.validator.addMethod("minimum2", function(value, element) {
+        
+        var category = document.getElementById("category_signup");
+        var values = getSelectValues(category);
+        return values.length >= 2;
+    }, "At least 2 categories");
+
 
     $('#sign_up_form').validate({ // initialize the plugin
         rules: {
             username: {
+                minlength: 3,
+                maxlength: 8,
                 required: true,
-                unique: true
+                letters: true
             },
             password: {
                 required: true,
-                minlength: 8,
+                minlength: 5,
+                maxlength:10,
                 lettersAndNumbers: true
             },
             fName: {
@@ -37,9 +39,25 @@ $(document).ready(function () {
                 required: true,
                 letters: true
             },
+            
             email: {
                 required: true,
                 email: true
+            },
+            country:{
+                required:true
+            },
+            Categories:{
+                minimum2: true
+            },
+            city:{
+                required:true
+            },
+            question:{
+                required:true
+            },
+            answer:{
+                required:true
             }
         },
         submitHandler: function (form) {
